f45332ed4fd5dd1edb7e54c567df5bdb3f51ea48
user <alimuradof93@gmail.com>
2025-08-04 23:51:11 +0400

    jobposting entity has been updated

--- src/main/java/az/turing/hiremeproject/entity/JobPosting.java
+++ src/main/java/az/turing/hiremeproject/entity/JobPosting.java
@@ -1,12 +1,11 @@
 package az.turing.hiremeproject.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Entity
@@ -15,12 +14,37 @@ public class JobPosting {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    private String description;
-    private String location;
+    @ElementCollection
+    @CollectionTable(name = "job_posting_description",
+            joinColumns = @JoinColumn(name = "job_posting_id"))
+    @Column(name = "description")
+    private List<String> description;
     private Boolean isActive;
     private LocalDate startDate;
     private LocalDate endDate;
     private String companyName;
     private String contactNumber;
     private String contactEmail;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String jobField;
+    @ElementCollection
+    @CollectionTable(name = "job_posting_requirements",
+        joinColumns = @JoinColumn(name = "job_posting_id"))
+    @Column(name = "requirement")
+    private List<String> requirements;
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        if (isActive == null) {
+            isActive = true;
+        }
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+
+
 }
